c3771511e2372b2181d1917c0da615792393a3e6
BuildTools <ealvarez2025@gmail.com>
2023-06-12 19:11:12 -0700

    Add xl media query for better reactiveness on small devices

--- app/components/OptionCard.tsx
+++ app/components/OptionCard.tsx
@@ -10,7 +10,9 @@ const OptionCard: React.FC<OptionCardProps> = ({icon: Icon, name}) => {
         <div className="cursor-pointer w-fit group">
             <div className="flex-col flex border-black border-4 rounded-lg w-fit group-hover:scale-110 transition">
                 <Icon size={200} className=""/>
-                <h2 className="text-center text-xl font-bold border-t-4 border-black break-normal max-w-[200px]">{name}</h2>
+                <div className="max-w-[200px] min-h-[4rem] flex-col flex justify-center border-t-4 border-black">
+                    <h2 className="text-center m-auto text-xl font-bold  break-normal">{name}</h2>
+                </div>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { getServerSession } from 'next-auth'
 import SessionButton from './components/SessionButton';
 import { getAccessToken, getCurrentUserPlaylist } from './lib/spotify';
 import Title from './components/Title';
+import OptionCard from './components/OptionCard';
+import {BiMedal} from 'react-icons/bi';
 
 export default async function Home() {
   const userSession = await getServerSession(authOptions);
@@ -11,11 +13,16 @@ export default async function Home() {
   if (userSession) {
     return (
       <main>
-        <div className='w-1/2 flex-col mx-auto'>
+        <div className='lg:w-1/2 flex-col mx-auto'>
           <Title text='Chartify' />
           <p className='text-center text-xl'>
             {`Welcome ${userSession.user?.name}, select an option below to get started!`}
           </p>
+          <div className='flex-wrap justify-around flex gap-4'>
+            <OptionCard icon={BiMedal} name="How Basic is my Music Taste?" />
+            <OptionCard icon={BiMedal} name="Genres" />
+            <OptionCard icon={BiMedal} name="How Basic is my Music Taste?" />
+          </div>
           <SessionButton session={userSession} />
         </div>
       </main>
@@ -25,7 +32,7 @@ export default async function Home() {
       <main>
         <div className='w-1/2 flex-col mx-auto'>
           <Title text='Chartify' />
-          <p className='text-center text-xl'>
+          <p className='text-center text-xl '>
             Log in with your Spotify account to get started!
           </p>
           <SessionButton session={userSession} />
