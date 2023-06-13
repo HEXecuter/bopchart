ba8ef919e3a8cc1061678e47a55dc30fb8486bae
BuildTools <ealvarez2025@gmail.com>
2023-06-12 17:07:18 -0700

    Customize SessionButton to include Spotify Icon

--- app/SessionButton.tsx
+++ app/SessionButton.tsx
@@ -2,19 +2,30 @@
 
 import { Session } from "next-auth";
 import { signIn, signOut } from 'next-auth/react'
+import { BsSpotify } from 'react-icons/bs'
 
 interface SessionButtonProps {
-    session?: Session
+    session: Session | null
 }
 
 const SessionButton: React.FC<SessionButtonProps> = ({ session }) => {
     if (session) {
         return (
-            <button onClick={()=> signOut()}>Sign out</button>
+            <button onClick={()=> signOut()}
+            className="rounded-xl bg-red-500 flex justify-center py-4 gap-6 w-full"
+            >
+                <BsSpotify size={32}/>
+                <span className="text-xl">{`Log out from ${session.user?.name}`}</span>
+            </button>
         )
     } else {
         return (
-            <button onClick={()=> signIn('spotify')}>Sign in</button>
+            <button onClick={()=> signIn('spotify')}
+            className="rounded-xl bg-green-500 flex justify-center py-4 gap-6 w-full"
+            >
+                <BsSpotify size={32}/>
+                <span className="text-xl">Log in with Spotify</span>
+            </button>
         )
     }
 
