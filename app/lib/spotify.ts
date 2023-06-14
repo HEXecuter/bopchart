76c187df4d1cb48151b4b3f9364fe711bdfa0e3e
BuildTools <ealvarez2025@gmail.com>
2023-06-14 00:25:25 -0700

    Create functions for retrieving playlist tracks and album data

--- app/lib/spotify.ts
+++ app/lib/spotify.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 const BASIC_TOKEN = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
 const CURRENT_PLAYLIST_ENDPOINT = 'https://api.spotify.com/v1/me/playlists'
+const BULK_ALBUM_ENDPOINT = 'https://api.spotify.com/v1/albums'
 
 export async function getAccessToken(accessToken: string) {
     const data = {
@@ -28,7 +29,7 @@ export async function getAccessToken(accessToken: string) {
 
 export async function getCurrentUserPlaylist(bearerToken: string) {
     const params = {
-        limit: 10,
+        limit: 50,
         offset: 0
     }
 
@@ -37,12 +38,55 @@ export async function getCurrentUserPlaylist(bearerToken: string) {
     let response = await axios.get(CURRENT_PLAYLIST_ENDPOINT, { headers: headers, params: params })
 
     let playlist_list: PlaylistItem[] = []
-    playlist_list = playlist_list.concat(response.data.items)
+    playlist_list.push(...response.data.items)
 
     while (response.data.next) {
         response = await axios.get(response.data.next, { headers: headers })
         playlist_list.push(...response.data.items)
     }
-    
+
     return playlist_list
+}
+
+export async function getAllTracksInPlaylist(playlistId: string, bearerToken: string) {
+    const playlistUrl = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`
+    const headers = { Authorization: `Bearer ${bearerToken}` }
+
+    let response = await axios.get(playlistUrl, { headers: headers })
+
+    let tracks_list: PlaylistTrackItem[] = []
+    tracks_list.push(...response.data.items)
+
+    while (response.data.next) {
+        response = await axios.get(response.data.next, { headers: headers })
+        tracks_list.push(...response.data.items)
+    }
+
+    return tracks_list;
+}
+
+export function getUniqueAlbumsFromTracks(trackList: PlaylistTrackItem[]) {
+    const albumIds = new Set();
+    for (const track of trackList) {
+        albumIds.add(track.track.album.id)
+    }
+
+    return [...albumIds];
+}
+
+export async function getBulkAlbums(albumList: string[], bearerToken: string) {
+    const headers = { Authorization: `Bearer ${bearerToken}` }
+    let currentBatch = []
+    const albums = new Map<string, FullAlbum>()
+
+    for (const id of albumList) {
+        currentBatch.push(id);
+        if (currentBatch.length == 20 || id == albumList.at(-1)) {
+            const response = await axios.get(BULK_ALBUM_ENDPOINT, { headers, params: { ids: currentBatch.join(',') } })
+            currentBatch = []
+            response.data.albums.forEach((album: FullAlbum) => albums.set(album.id, album))
+        }
+    }
+
+    return albums;
 }
