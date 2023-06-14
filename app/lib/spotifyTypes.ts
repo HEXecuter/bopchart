5bc32f21b6733c3555ae13df82bf0bbc4c0946e5
BuildTools <ealvarez2025@gmail.com>
2023-06-13 17:05:45 -0700

    Add types for tracks in playlists

--- app/lib/spotifyTypes.ts
+++ app/lib/spotifyTypes.ts
@@ -6,6 +6,7 @@ type PlaylistItem = {
     id: string,
     images: SpotifyImage[],
     name: string,
+    owner: SpotifyOwner
     public: boolean
     snapshot_id: string,
     tracks: {href: string, total: number}[],
@@ -13,8 +14,66 @@ type PlaylistItem = {
     uri: string
 }
 
+type TrackItem = {
+    added_at: string,
+    added_by: Omit<SpotifyOwner, 'display_name'>,
+    is_local: boolean,
+    primary_color: null,
+    track: Track
+}
+
+
+type Track = {
+    album: Album,
+    artists: Artist,
+    available_markets: string[],
+    disc_number: number,
+    duration_ms: number,
+    episode: boolean,
+    explicit: boolean,
+    href: string,
+    id: string,
+    is_local: boolean,
+    name: string,
+    popularity: number,
+    preview_url: string,
+    track: boolean,
+    track_number: number,
+    type: "track"
+}
+
+type Album = {
+    album_type: "album",
+    artists: Artist[],
+    available_markets: string[]
+    href: string,
+    id: string,
+    images: SpotifyImage[],
+    name: string,
+    release_date: string,
+    total_tracks: number,
+    type: "album",
+    uri: string
+}
+
+type Artist = {
+    href: string,
+    id: string,
+    name: string,
+    type: "artist",
+    uri: string
+}
+
 type SpotifyImage = {
     url: string,
     height: number,
     width: number,
+}
+
+type SpotifyOwner = {
+    display_name: string,
+    href: string,
+    id: string,
+    type: "user",
+    uri: string
 }
