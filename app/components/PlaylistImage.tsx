f32f1768099de14c5844af070cf0e0154dd8f253
BuildTools <ealvarez2025@gmail.com>
2023-06-20 15:20:20 -0700

    Redesign PlaylistSelector component to comply with Spotify's design guidelines

--- app/components/PlaylistImage.tsx
+++ app/components/PlaylistImage.tsx
@@ -8,14 +8,15 @@ interface PlaylistImageProps {
 const PlaylistImage: React.FC<PlaylistImageProps> = ({ playlistItem }) => {
     if(playlistItem.images.length > 0) {
         return (
-            <Image src={playlistItem.images[0].url} height={320} width={320} alt="Album cover"
-                style={{ width: "320px", height: "320px", objectFit: "cover" }}
+            <Image src={playlistItem.images[0].url} height={180} width={180} alt="Album cover"
+                // style={{ width: "320px", height: "320px", objectFit: "cover" }}
+                className="w-[100px] h-[100px] lg:w-[160px] lg:h-[160px] object-cover"
             />
         )
     } else {
         return (
-            <div className="w-[320px] h-[320px] bg-gray-900 flex-col flex items-center justify-center">
-                <LuMusic size={100} className="text-gray-600"/>
+            <div className="w-[100px] h-[100px] lg:w-[160px] lg:h-[160px] bg-gray-900 flex-col flex items-center justify-center">
+                <LuMusic size={50} className="text-gray-600"/>
             </div>
         )
     }
--- app/components/PlaylistSelector.tsx
+++ app/components/PlaylistSelector.tsx
@@ -1,9 +1,9 @@
 import { Session } from "next-auth";
 import { getAccessToken, getCurrentUserPlaylist } from "../lib/spotify";
 import Link from "next/link";
-import Image from 'next/image';
 import Title from "./Title";
 import PlaylistImage from "./PlaylistImage";
+import { BsSpotify } from 'react-icons/bs'
 
 interface PlaylistSelectorProps {
     userSession: Session
@@ -21,16 +21,23 @@ const PlaylistSelector: React.FC<PlaylistSelectorProps> = async ({ userSession, 
             <div className="flex-wrap flex justify-center items-center gap-6 my-6">
                 {playlistList.map((playlistItem) => {
                     return (
-                        <div key={playlistItem.id} className="max-w-[320px] overflow-clip text-ellipsis whitespace-nowrap
-                        bg-[rgba(255,255,255,0.2)] box-content px-6
+                        <div key={playlistItem.id} className=" flex items-center gap-3 bg-gray-950 p-4 rounded-xl
                         ">
-                            <span className="text-lg">{playlistItem.name}</span>
                             <Link href={`${baseUrlPath}?playlistId=${playlistItem.id}`}>
                                 <PlaylistImage playlistItem={playlistItem} />
                             </Link>
-                            <a href={playlistItem.external_urls.spotify} target="_blank">
-                                <small className="underline">Listen on Spotify</small>
-                            </a>
+                            <div className="flex-col flex items-start justify-center h-20 lg:h-[160px] w-32 lg:w-[20rem] gap-1 lg:gap-3
+                            overflow-clip text-ellipsis whitespace-nowrap
+                            ">
+                                <h2 className="text-base lg:text-3xl font-normal">{playlistItem.name || "Unnamed"}</h2>
+                                <p className="text-gray-300 text-sm lg:text-xl">{playlistItem.owner.display_name || "Unknown"}</p>
+                                <a href={playlistItem.external_urls.spotify} target="_blank" className="flex gap-2 items-end underline">
+                                    <BsSpotify size={25} className="" />
+                                    <p className="text-sm lg:text-base">Play on Spotify</p>
+                                </a>
+                                
+                            </div>
+
                         </div>
                     )
                 })}
